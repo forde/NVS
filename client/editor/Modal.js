0a778afc6e660139d701d68e21368b34324f2896
konrad <konrad@kodebyraaet.no>
2022-05-05 10:49:26 +0200

    optional portal support added to Modal

--- client/editor/Modal.js
+++ client/editor/Modal.js
@@ -3,8 +3,18 @@ import { styled } from 'linaria/react'
 import { MdCancel } from 'react-icons/md'
 
 import Button from './Button'
+import Portal from '/editor/lib/Portal'
 
-export default function Modal ({ children, onClose, width, height, className, white, style }) {
+export default function Modal ({
+    children,
+    onClose,
+    width,
+    height,
+    usePortal=false,
+    className,
+    white,
+    style
+}) {
 
     useEffect(() => {
         const onKeyup = e => e.key === 'Escape' ? onClose() : null
@@ -16,29 +26,23 @@ export default function Modal ({ children, onClose, width, height, className, wh
         }
     }, [])
 
+    const Wrapper = usePortal ? Portal : 'div'
+
     return (
-        <Wrapper id="modal" onClick={e => onClose && e.target.id === 'modal' ? onClose() : null} white={white}>
-            <div>
-                <ModalEl className={className} id="modal-window" width={width} height={height} white={white} style={style} >
-                    {children}
-                </ModalEl>
-                <Button className="close-icon" tertiary small icon={MdCancel} onClick={onClose} />
-            </div>
+        <Wrapper>
+            <Overlay id="modal" onClick={e => onClose && e.target.id === 'modal' ? onClose() : null} white={white}>
+                <div>
+                    <ModalEl className={className} id="modal-window" width={width} height={height} white={white} style={style} >
+                        {children}
+                    </ModalEl>
+                    <Button className="close-icon" tertiary small icon={MdCancel} onClick={onClose} />
+                </div>
+            </Overlay>
         </Wrapper>
     )
 }
 
-const Wrapper = styled.div`
-    > div {
-        position:relative;
-    }
-    .close-icon {
-        position: absolute;
-        top: 16px;
-        right: 16px;
-        cursor: pointer;
-        z-index: 900;
-    }
+const Overlay = styled.div`
     position:fixed;
     top:0;
     left:0;
@@ -50,6 +54,16 @@ const Wrapper = styled.div`
     align-items: center;
     justify-content: center;
     cursor: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABGdBTUEAALGPC/xhBQAAAQBJREFUSA2tldENgjAQQIkJI8AejuAE/jML/25jHMT45QYO4AIm+K56eiS00F4vuVwJ6XttaWnTfGOapp22a1TlBSgPR6BXal8JPsI5w2sbMZE3UuJOuiT0HwVEvMhDGDCNnhS4RLGEvhY+zFaDly5JEq6mUskmeKkkC54rKYJvlbjga5Iq8ITkhEBC9vl8K2qn3ArIbuG6cB0MVB25CB6k68QrN1Rg9oQKXKL4xKfgA2C7XD4JMDvy3wetIonBdXouyRo8Ien0XbRuhStgYSZxSS48S1IKN5IOhr20/jPxwpMS4HInX8gq/xY4ek6etPdBTqMlPxe0DsdRYclyBfgbXpalja44wn4AAAAASUVORK5CYII='), auto;
+    > div {
+        position:relative;
+    }
+    .close-icon {
+        position: absolute;
+        top: 16px;
+        right: 16px;
+        cursor: pointer;
+        z-index: 900;
+    }
 `
 
 const ModalEl = styled.div`
